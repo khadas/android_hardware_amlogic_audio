f56ef622e2b24cd80d8e226da970a0f287cc4fa0
yujie.wu <yujie.wu@amlogic.com>
2020-09-16 18:33:45 +0800

    audio: Fix ARC audio break issue [1/1]
    PD#SWPL-32214
    
    Problem:
      When play ac3 file with arc output, sometimes
      there is no sound or break at the beginning.
    
    Solution:
      Config the alsa info according to continuous mode
    
    Verify:
      marconi
    
    Change-Id: I424c4566227b7b101e3f899f79b93d015c3b0cb7
    Signed-off-by: yujie.wu <yujie.wu@amlogic.com>

--- audio_hal/alsa_config_parameters.c
+++ audio_hal/alsa_config_parameters.c
@@ -79,14 +79,22 @@ static void get_ddp_hardware_config_parameters(
 static void get_dd_hardware_config_parameters(
     struct pcm_config *hardware_config
     , unsigned int channels __unused
-    , unsigned int rate)
+    , unsigned int rate
+    , bool continuous_mode)
 {
     hardware_config->channels = 2;
     hardware_config->format = PCM_FORMAT_S16_LE;
     hardware_config->rate = rate;
-    hardware_config->period_count = PLAYBACK_PERIOD_COUNT * 2;
     hardware_config->period_size = PERIOD_SIZE;
-    hardware_config->start_threshold = hardware_config->period_size * hardware_config->period_count / 4;
+
+    if (continuous_mode) {
+        hardware_config->period_count = PLAYBACK_PERIOD_COUNT * 2;
+        hardware_config->start_threshold = hardware_config->period_size * hardware_config->period_count / 4;
+    } else {
+        hardware_config->period_count = PLAYBACK_PERIOD_COUNT;
+        hardware_config->start_threshold = hardware_config->period_size * hardware_config->period_count / 2;
+    }
+
     hardware_config->avail_min = 0;
 
     return ;
@@ -149,11 +157,11 @@ int get_hardware_config_parameters(
     }
     //DD
     else if (output_format == AUDIO_FORMAT_AC3) {
-        get_dd_hardware_config_parameters(final_config, channels, rate);
+        get_dd_hardware_config_parameters(final_config, channels, rate, continuous_mode);
     }
     //DTS-HD/TRUE-HD
     else if ((output_format == AUDIO_FORMAT_DTS_HD) || (output_format == AUDIO_FORMAT_DOLBY_TRUEHD)) {
-        get_dd_hardware_config_parameters(final_config, channels, rate);
+        get_dd_hardware_config_parameters(final_config, channels, rate, continuous_mode);
     }
     //PCM
     else {
