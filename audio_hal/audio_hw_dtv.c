45641bce58b146f5f33af7bf7dfd31635309e95f
Rui Wang <rui.wang@amlogic.com>
2021-07-06 19:57:58 +0800

    AudioRebase: support audio tunnel mode [1/1]
    PD#SWPL-52556
    
    Problem:
    porting R DTV audio.
    
    Solution:
    modify devid for mediasync_wrap_setParameter.
    
    Verify:
    smith
    
    Signed-off-by: Rui Wang <rui.wang@amlogic.com>
    Change-Id: I2cfb6558af4863a6d6131655bdbf76330e22aa1d

--- audio_hal/audio_hw_dtv.c
+++ audio_hal/audio_hw_dtv.c
@@ -393,7 +393,7 @@ int dtv_patch_handle_event(struct audio_hw_device *dev,int cmd, int val) {
                             mediasync_wrap_setParameter(dtvsync->mediasync_new, MEDIASYNC_KEY_ISOMXTUNNELMODE, &audio_sync_mode);
                             mediasync_wrap_bindInstance(dtvsync->mediasync_new, dtvsync->mediasync_id, MEDIA_AUDIO);
                             ALOGI("normal output version CMD open audio bind syncId:%d\n", dtvsync->mediasync_id);
-                            mediasync_wrap_setParameter(dtvsync, MEDIASYNC_KEY_HASAUDIO, &has_audio);
+                            mediasync_wrap_setParameter(dtvsync->mediasync_new, MEDIASYNC_KEY_HASAUDIO, &has_audio);
                         }
                     }
                     ALOGI("create mediasync:%p\n", dtvsync->mediasync_new);
