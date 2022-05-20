6b53597cc57ffdf439fec88ee403c21aea58c68e
lianlian zhu <lianlian.zhu@amlogic.com>
2022-05-20 16:34:14 +0800

    audio: fixed automotive audiohal compile issue [1/1]
    PD#SWPL-78263
    
    Problem:
    automotive audiohal compile issue
    
    Solution:
    All dtv related code need in  macro definition
    of ENABLE_DVB_PATCH.
    
    Verify:
    Verify on A311D2.
    
    Signed-off-by: lianlian zhu <lianlian.zhu@amlogic.com>
    Change-Id: Idddefb30c69e2c7cac167520f6adb1565ed24836

--- audio_hal/audio_hw.c
+++ audio_hal/audio_hw.c
@@ -7542,6 +7542,7 @@ ssize_t out_write_new(struct audio_stream_out *stream,
     if (adev->debug_flag > 1) {
         ALOGI("+<IN>%s: out_stream(%p) position(%zu)", __func__, stream, bytes);
     }
+#ifdef ENABLE_DVB_PATCH
 #if ANDROID_PLATFORM_SDK_VERSION > 29
     if (aml_out && adev &&
         (aml_out->dev->patch_src == SRC_DTV) &&
@@ -7561,6 +7562,8 @@ ssize_t out_write_new(struct audio_stream_out *stream,
         return bytes;
     }
 #endif
+#endif
+
 
     if (aml_audio_trace_debug_level() > 0) {
         if (false == aml_out->pause_status  &&  aml_out->write_count < 1) {
