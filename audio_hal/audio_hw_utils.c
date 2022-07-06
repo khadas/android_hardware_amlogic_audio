ec5bd75dce8f7aa9cc0e2f1a6b67d7b0598c1dc1
lianlian zhu <lianlian.zhu@amlogic.com>
2022-07-06 16:58:13 +0800

    audio: fixed cbs security audio no sound issue [1/1]
    PD#SWPL-87178
    
    Problem:
    DVB plays SMP scrambled program,
    the program has video but no audio output
    
    Solution:
    1.add api to set security level
    2.adapt aac format for aac and latm
    
    Verify:
    sc2
    
    Signed-off-by: lianlian zhu <lianlian.zhu@amlogic.com>
    Change-Id: I0dd3de222be78a1372dd67b205ef92203e048699

--- audio_hal/audio_hw_utils.c
+++ audio_hal/audio_hw_utils.c
@@ -1663,9 +1663,9 @@ int android_fmt_convert_to_dmx_fmt(audio_format_t android_fmt) {
         case AUDIO_FORMAT_AAC_MAIN:
         case AUDIO_FORMAT_AAC_LC:
         case AUDIO_FORMAT_AAC_HE_V2:
-        case AUDIO_FORMAT_AAC_HE_V1:
             return ACODEC_FMT_AAC;
-
+        case AUDIO_FORMAT_AAC_HE_V1:
+            return ACODEC_FMT_AAC_LATM;
         case AUDIO_FORMAT_AC3:
             return ACODEC_FMT_AC3;
 
