0a8b84a1d4d405235593966c0c9748c5873f26d4
Cheng Wang2 <cheng.wang2@amlogic.com>
2022-09-21 21:13:03 +0800

    T7C: Android T bring up [1/1]
    PD#SWPL-99088
    
    Problem:
    device initialize
    
    Solution:
    solute compile error
    
    Verify:
    T7
    
    Signed-off-by: Cheng Wang2 <cheng.wang2@amlogic.com>
    Change-Id: Ia6fd7060ff2bca954ec9460d1293bb587a1f235e

--- dtv_audio_utils/Android.mk
+++ dtv_audio_utils/Android.mk
@@ -47,7 +47,7 @@ LOCAL_SRC_FILES  +=               \
     sync/audio_dtv_sync.c \
 
 LOCAL_MODULE := libdvbaudioutils
-LOCAL_MULTILIB := both
+LOCAL_MULTILIB := 32
 ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
     LOCAL_PROPRIETARY_MODULE := true
 endif
