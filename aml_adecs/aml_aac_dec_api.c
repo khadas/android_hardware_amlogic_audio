d3bd312265c6fe2a33f84f0d4fb334633737ad1f
binbin dai <binbin.dai@amlogic.com>
2023-07-10 15:33:26 +0800

    audio: drag the progress bar, and the screen will freeze for a while [1/2]
    PD#SWPL-131178
    
    Problem:
    drag the progress bar, and the screen will freeze for a while.
    
    Solution:
    1.libfaad cache too much data when audio decode init, so optimistic
    this logic.
    2. The framesize of LATM parse from header which is too big than buffer
    size, if so, next check.
    3.Store the data in time and decode the data in time which will cause the
    apts jump.
    
    Verify:
    U ohm
    T5M S
    
    Change-Id: I88860bb337201c239b9dcee83b40ec6a9c996236
    Signed-off-by: binbin dai <binbin.dai@amlogic.com>

--- aml_adecs/aml_aac_dec_api.c
+++ aml_adecs/aml_aac_dec_api.c
@@ -381,9 +381,8 @@ static int faad_decoder_process(aml_dec_t *aml_dec, unsigned char *buffer, int b
     while (aac_dec->remain_size >  used_size) {
       int pcm_len = AAC_MAX_LENGTH;
       int decode_len = faad_op->decode(faad_op, (char *)(dec_pcm_data->buf + dec_pcm_data->data_len), &pcm_len, (char *)aac_dec->remain_data + used_size, aac_dec->remain_size  - used_size);
-
+      used_size += decode_len;
       if (pcm_len > 0) {
-          used_size += decode_len;
           dec_pcm_data->data_len += pcm_len;
           if (dec_pcm_data->data_len > dec_pcm_data->buf_size) {
               ALOGE("decode len %d  > buf_size %d ", dec_pcm_data->data_len, dec_pcm_data->buf_size);
