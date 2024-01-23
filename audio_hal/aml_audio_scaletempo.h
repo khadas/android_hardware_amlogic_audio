b8a3169cf9acd1585f0fe85eb57e71a09325edf9
yujie.wu <yujie.wu@amlogic.com>
2024-01-23 14:39:26 +0800

    audio: Fix speed function crash issue [1/1]
    PD#SWPL-154656
    
    Problem:
    When use 64bit so, there is crash in speed function
    
    Solution:
    Align the data type for nstride, it should use ptrdiff_t
    which is same in ms12 so
    
    Verify:
    t7c
    
    Change-Id: I0d3fca8953bdd91637bbf01bb8a84091d15f96b6
    Signed-off-by: yujie.wu <yujie.wu@amlogic.com>

--- audio_hal/aml_audio_scaletempo.h
+++ audio_hal/aml_audio_scaletempo.h
@@ -112,7 +112,7 @@ typedef struct scale_tempo_buffer_s
      *  unsigned char. For interleaved audio data, set this equal to nchannel.
      *  For separate channel buffers, set this to 1.
      */
-    unsigned   nstride;
+    ptrdiff_t   nstride;
 
     /** One of the DLB_BUFFER_* codes */
     int         data_type;
