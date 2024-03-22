823b04b86840dd83de2cd8b37e4958e847d12c65
dehong.chen <dehong.chen@amlogic.com>
2024-03-22 11:28:04 +0000

    audio: HDMI AVsync test has failed [1/1]
    PD#SWPL-160040
    PD#SWPL-160683
    
    Problem:
    audio is output late than video
    
    Solution:
    adjust audio output latency
    
    Verify:
    S7
    
    Change-Id: I5cfea0b99c59265c5eae80dc9175e0eb8fcf4f5f
    Signed-off-by: dehong.chen <dehong.chen@amlogic.com>

--- audio_hal/audio_avsync_table_aml_ms12_v2.h
+++ audio_hal/audio_avsync_table_aml_ms12_v2.h
@@ -459,10 +459,10 @@
 #define  AVSYNC_MS12_DTV_HDMI_ARC_OUT_PCM_LATENCY            (50)
 #define  AVSYNC_MS12_DTV_HDMI_ARC_OUT_DD_LATENCY             (0)
 #define  AVSYNC_MS12_DTV_HDMI_ARC_OUT_DDP_LATENCY            (0)
-#define  AVSYNC_MS12_DTV_HDMI_OUT_PCM_LATENCY                (-68) /* if 0,  result locates at [-10, 10] */
+#define  AVSYNC_MS12_DTV_HDMI_OUT_PCM_LATENCY                (-60) /* if 0,  result locates at [-10, 10] */
 #define  AVSYNC_MS12_DTV_HDMI_OUT_DD_LATENCY                 (0)
 #define  AVSYNC_MS12_DTV_HDMI_OUT_DDP_LATENCY                (-80)
-#define  AVSYNC_MS12_DTV_HDMI_OUT_MAT_LATENCY                (-25)
+#define  AVSYNC_MS12_DTV_HDMI_OUT_MAT_LATENCY                (-40)
 #define  AVSYNC_MS12_DTV_SPEAKER_LATENCY                     (0)
 #define  AVSYNC_MS12_TV_DTV_SPEAKER_LATENCY                  (60)
 
@@ -540,7 +540,7 @@
 
 
 /* for different output format */
-#define  AVSYNC_NONMS12_DTV_PCM_OUT_LATENCY                     (0)
+#define  AVSYNC_NONMS12_DTV_PCM_OUT_LATENCY                     (-25)
 #define  AVSYNC_NONMS12_DTV_DD_OUT_LATENCY                      (0)
 #define  AVSYNC_NONMS12_DTV_DDP_OUT_LATENCY                     (-64)
 #define  AVSYNC_NONMS12_DTV_MAT_OUT_LATENCY                     (0)
