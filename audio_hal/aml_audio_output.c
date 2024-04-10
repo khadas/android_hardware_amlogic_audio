961f9e55bd5fbe3ad8cc06fc9d7f1194c98499f0
lianlian zhu <lianlian.zhu@amlogic.com>
2024-04-10 15:21:12 +0800

    audio: fixed dtv dc on/off crash issue [1/1]
    PD#SWPL-164651
    
    Problem:
    Perform DC off/on stress test,
    /vendor/bin/hw/android.hardware.audio.service-droidlogic
    crash Frequency:1/1pcs/12H
    
    Solution:
    audiopatch manager and write data is asynchronous,
    so should not use audiopatch to control the sound mode
    process.
    
    Verify:
    t5m
    
    Change-Id: I1eb4fd6db81215f585bf846cfa21f010ceb17514
    Signed-off-by: lianlian zhu <lianlian.zhu@amlogic.com>

--- audio_hal/aml_audio_output.c
+++ audio_hal/aml_audio_output.c
@@ -471,7 +471,7 @@ ssize_t audio_hal_data_processing(struct audio_stream_out *stream,
         } else {
             if (is_same_patch_src(adev, SRC_DTV) && is_dev_patch_exist(adev)) {
                 if (is_dolby_ms12_support_compression_format(aml_out->hal_internal_format))  {
-                    aml_audio_switch_output_mode((int16_t *)buffer, bytes, AUDIO_FORMAT_PCM_16_BIT, get_dev_patch(adev)->mode);
+                    aml_audio_switch_output_mode((int16_t *)buffer, bytes, AUDIO_FORMAT_PCM_16_BIT, adev->sound_track_mode);
                 }
             } else if (!is_dev_patch_exist(adev)) {
                 aml_audio_switch_output_mode((int16_t *)buffer, bytes, AUDIO_FORMAT_PCM_16_BIT, adev->sound_track_mode);
