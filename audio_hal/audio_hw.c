4ccfef7c3d914705599a72c20117f6e8225dbe5c
zeming.huang <zeming.huang@amlogic.com>
2024-05-07 17:38:11 +0800

    audio: system audio pcm use line mode [1/1]
    PD#SWPL-166738
    
    Problem:
    ms12's sound of boot up is louder than non-dolby,
    for drc mode is RF mode.
    
    Solution:
    system audio pcm use line mode
    
    Verify:
    T5D U
    
    Change-Id: Iabe781694facd12f62afa500b5e54981d263f257
    Signed-off-by: zeming.huang <zeming.huang@amlogic.com>

--- audio_hal/audio_hw.c
+++ audio_hal/audio_hw.c
@@ -6555,8 +6555,8 @@ ssize_t mixer_aux_buffer_write(struct audio_stream_out *stream, const void *buff
 
 #ifndef AUDIO_HAL_DISABLE_MS12
         // NTS PCM mode: volume-tunel-nontunel/audio-lat-heaac testcase.
-        if (adev->is_netflix && (eDolbyMS12Lib == adev->dolby_lib_type) && !dolby_stream_active(adev)) {
-            ALOGI("%s : without dolby_stream, netflix pcm drc use line mode", __func__);
+        if ((eDolbyMS12Lib == adev->dolby_lib_type) && !dolby_stream_active(adev)) {
+            ALOGI("%s : without dolby_stream, pcm drc use line mode", __func__);
             set_ms12_drc_params_for_stereo_and_dap_multi_pcm_output(
                 adev
                 , ms12
