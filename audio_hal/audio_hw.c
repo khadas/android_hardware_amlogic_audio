d1a32842c9d5d53c047f47a9f14d2c552d71b7df
yongbiao.ma <yongbiao.ma@amlogic.com>
2024-06-21 09:33:26 +0000

    audio: fix ms12 case crash issue. [1/1]
    PD#SWPL-173191
    
    Problem:
    ms12 case crash.
    
    Solution:
    Only send signals to submix when nonms12 case.
    
    Verify:
    Android U
    
    Change-Id: I4143bed39fa154475d62605613fd693b00bf50f2
    Signed-off-by: yongbiao.ma <yongbiao.ma@amlogic.com>

--- audio_hal/audio_hw.c
+++ audio_hal/audio_hw.c
@@ -3387,7 +3387,9 @@ static int adev_open_output_stream(struct audio_hw_device *dev,
         bool request_llp_mode = false;
         ALOGI("when open aaudio stream, send RUNNING msg to submix & ms12");
         aml_audiohal_sch_state_2_ms12(ms12, MS12_SCHEDULER_RUNNING);
-        aml_audiohal_sch_state_2_submix(audio_mixer, SUBMIX_SCHEDULER_RUNNING);
+        if (adev->useSubMix) {
+           aml_audiohal_sch_state_2_submix(audio_mixer, SUBMIX_SCHEDULER_RUNNING);
+        }
         if (outMmapInit(out) != 0) {
             AM_LOGE("outMmapInit out %p fail !", out);
             ret = -1;
@@ -3584,8 +3586,10 @@ static void adev_close_output_stream(struct audio_hw_device *dev,
         }
 #endif
         ALOGI("when close aaudio stream, send STANDBY msg to submix & ms12");
-        aml_audiohal_sch_state_2_submix(audio_mixer, SUBMIX_SCHEDULER_STANDBY);
         aml_audiohal_sch_state_2_ms12(ms12, MS12_SCHEDULER_STANDBY);
+        if (adev->useSubMix) {
+            aml_audiohal_sch_state_2_submix(audio_mixer, SUBMIX_SCHEDULER_STANDBY);
+        }
     }
 
     if (out->hal_format == AUDIO_FORMAT_AC4) {
